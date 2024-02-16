181bc23375895f84829d78b71b62c518b6a6fed3
unknown <sahil.kundan48@gmail.com>
2024-02-16 16:40:33 +0530

    first

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,6 +6,25 @@ app.get('/', (req, res) => {
     res.send("Server is Ready");
 });
 
+// get a list of 1 jokes
+
+app.get('/jokes', (req,res) => {
+
+    const jokes = [
+        {
+            id:1,
+            title: 'A Joke',
+            content: 'This is a Joke'
+        },
+        {
+            id:2,
+            title: 'Another Joke',
+            content: 'This is a Another Joke'
+        }
+    ];
+    res.send(jokes);
+})
+
 const port = process.env.PORT || 3000; // Corrected to use PORT instead of port
 
 app.listen(port, () => {
