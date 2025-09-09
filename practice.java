0b731f4f311f0e179fad7832c777b6c990cdb096
Sahil Kundan <sahilkundan@Sahils-MacBook-Air.local>
2025-09-09 22:26:04 +0530

    Arranged Codes

--- practice.java
+++ practice.java
@@ -1,27 +1,35 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 public class practice {
-    public static void callmoveoptimised(int[] n){
+    public static void callfun(String s){
 
-        int count =0;
+        Stack<Character> st = new Stack<>();
 
-        for(int i=0; i<n.length;i++){
-            if(n[i]!=0){
-                n[count] = n[i];
-                count++;
+        for(char ch: s.toCharArray()){
+            if(ch=='{' || ch=='[' || ch=='('){
+                st.push(ch);
+            }else if(ch==')' && !st.isEmpty() && st.peek()=='('){
+                st.pop();
+            }else if(ch==']' && !st.isEmpty() && st.peek()=='['){
+                st.pop();
+            }else if(ch=='}' && !st.isEmpty() && st.peek()=='{'){
+                st.pop();
+            }else{
+                System.out.println("String do not have valid Paranthesis");
+                break;
             }
         }
-            while(count<n.length){
-                n[count]=0;
-                count++;
-            }
-        
-       System.out.println(Arrays.toString(n));
+        if(st.isEmpty()){
+            System.out.println("String have valid Paranthesis");
+        }
+    
         
     }
     public static void main(String[] args) {
-        int[] nums1 = {0,1,0,3,12};
-        callmoveoptimised(nums1);
+        String s = "{[()]}";
+
+        callfun(s);
     }
 }
 
