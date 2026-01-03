2e6f330ce2622e15167fe062991c9d23fb674937
Sahil Kundan <sahilkundan@Sahils-MacBook-Air.local>
2026-01-03 16:05:25 +0530

    Selenium Codes

--- demo/src/test/java/OpenBrowserTest.java
+++ demo/src/test/java/OpenBrowserTest.java
@@ -1,4 +1,4 @@
-package com.qa.newPro;
+
 import java.util.Iterator;
 import java.util.List;
 import org.openqa.selenium.By;
@@ -23,6 +23,7 @@ public class OpenBrowserTest {
     }
 
     @Test (enabled = false)
+    //
     public void openBrowser() {
 
        List<WebElement> links= driver.findElements(By.tagName("a"));
@@ -42,6 +43,27 @@ public class OpenBrowserTest {
         WebElement tables = driver.findElement(By.xpath("//a[@href='/tables']"));
         tables.click();
 
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
+        Iterator<WebElement> itrows = rows.iterator();
+
+        int rowcount =1;
+
+        while (itrows.hasNext()) {
+            WebElement row = itrows.next();
+            List<WebElement> columnss = row.findElements(By.tagName("td"));
+            Iterator<WebElement> itcolumns = columnss.iterator();
+            int coloumncount =1;
+
+            while(itcolumns.hasNext()){
+                WebElement column = itcolumns.next();
+
+                if(rowcount==2 && coloumncount==2){
+                    System.out.println("Value: " + column.getText());
+                }
+                coloumncount++;
+            }
+            rowcount++;
+        }
     }
 
     @AfterMethod
