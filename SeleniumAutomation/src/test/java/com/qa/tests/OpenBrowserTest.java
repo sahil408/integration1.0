a2e8af59517450e6a9e4fabdd531629f826f977f
Sahil Kundan <sahilkundan@Sahils-MacBook-Air.local>
2026-01-15 00:27:23 +0530

    Only Selenium Codes

---
+++ SeleniumAutomation/src/test/java/com/qa/tests/OpenBrowserTest.java
@@ -0,0 +1,235 @@
+package com.qa.tests;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class OpenBrowserTest {
+    WebDriver driver;
+
+    @Parameters("url")
+    // @BeforeMethod(alwaysRun = true)
+    public void SetUp(@Optional("https://www.bing.com/search") String url) {
+        //WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().window().maximize();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void SetUpShoppingURL() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/tables");
+        driver.manage().window().maximize();
+    }
+
+    // @BeforeMethod(alwaysRun = true)
+    public void SetUpBingURL() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.bing.com/search/");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("selenium");
+        searchBox.submit();
+        //searchBox.sendKeys(Keys.RETURN);
+        driver.manage().window().maximize();
+    }
+
+    @Test (enabled = false)
+    // To get link and store all links's text
+    public void openBrowser() {
+
+       List<WebElement> links= driver.findElements(By.tagName("a"));
+       System.out.println("Total Links:" + links.size());
+
+       Iterator<WebElement> iterator = links.iterator();
+
+       while (iterator.hasNext()) {
+            WebElement link = iterator.next();
+            System.out.println(link.getText());  
+       }
+    }
+
+    @Test (enabled = false)
+    // To get table text in row 3 and column 2
+    public void selectTable() {
+
+        WebElement tables = driver.findElement(By.xpath("//a[@href='/tables']"));
+        //driver.findElement(By.cssSelector("a[href='/tables']"));
+        tables.click();
+
+        List<WebElement> rows = driver.findElements(By.xpath("//table[@id='table1']/tbody/tr"));
+        Iterator<WebElement> itrows = rows.iterator();
+
+        int rowcount =1;
+
+        while (itrows.hasNext()) {
+            WebElement row = itrows.next();
+            List<WebElement> columnss = row.findElements(By.tagName("td"));
+            Iterator<WebElement> itcolumns = columnss.iterator();
+            int coloumncount =1;
+
+            while(itcolumns.hasNext()){
+                WebElement column = itcolumns.next();
+
+                if(rowcount==2 && coloumncount==2){
+                    System.out.println("Value: " + column.getText());
+                }
+                coloumncount++;
+            }
+            rowcount++;
+        }
+    }
+
+    @Test (priority = 1, groups = "smoke", dependsOnMethods = "openBrowser", enabled = false)
+    // Selecting Dropdown
+    public void dropdown(){
+        driver.findElement(By.xpath("//a[@href='/dropdown']")).click();;
+
+        WebElement wb = driver.findElement(By.id("dropdown"));
+        
+
+        Select sl = new Select(wb);
+
+        sl.selectByIndex(1);
+        //sl.selectByValue("Option 2");
+
+
+        List<WebElement> dropdowns =  sl.getOptions();
+
+        for(WebElement option: dropdowns){
+            System.out.println(option.getText());
+        }
+    }
+
+    @Test (enabled = false)
+    // Fetching today's date and Time, Week of Year
+    public void DateandTime(){
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+
+        LocalDate d = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formatted = d.format(formatter);
+        System.out.println(formatted);
+
+        int weekofyear = d.get(WeekFields.of(Locale.getDefault()).weekOfYear());
+        System.out.println(weekofyear);
+
+    }
+
+    @Test (groups = "smoke", enabled = false)
+    public void mouseHoverAndKeyBoards(){
+        WebElement wb = driver.findElement(By.xpath("//a[@href='/hovers']"));
+        wb.click();
+        WebElement image = driver.findElement(By.xpath("(//div[@class='figure'])[3]"));
+        Actions action = new Actions(driver);
+        action.moveToElement(image).perform();
+
+        // Drag anD Drop
+
+        // WebElement source = driver.findElement(By.id("column-a"));
+        // WebElement target = driver.findElement(By.id("column-b"));
+
+        // Actions actions = new Actions(driver);
+        // actions.dragAndDrop(source, target).perform();
+
+        // Send Keys
+
+       // actions.sendKeys(Keys.ENTER).perform();
+       // actions.sendKeys(Keys.TAB/COMMAND/CONTROL).perform();
+
+
+       // CTRL +A
+        //    actions.keyDown(Keys.CONTROL)
+        //    .sendKeys("a")
+        //    .keyUp(Keys.CONTROL)
+        //    .perform();
+
+
+    }
+
+    @Test (groups = "smoke", enabled = false)
+    // Adding a item which is more than rs 500 to cart
+    public void additems(){
+        // String s = driver.getTitle();
+        // System.out.println(s);
+        driver.findElement(By.xpath("//a[@href='/products']")).click();
+        List<WebElement> wb = driver.findElements(By.xpath("//div[@class='productinfo text-center']"));
+
+        for(WebElement product: wb){
+            String pricetext = product.findElement(By.tagName("h2")).getText();
+            int price = Integer.parseInt(pricetext.replace("Rs. ", ""));
+
+            if(price>500){
+                product.findElement(By.xpath("//a[contains(text(),'Add to cart')]")).click();
+                System.out.println("Added product with price: Rs " + price);
+                break;
+                //driver.findElement(By.xpath("//button[contains(text(),'Continue Shopping')]")).click();
+            }
+        }
+    }
+
+    @Test (groups = "smoke",enabled = false)
+    public void bingurl(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+        By.xpath("//ol[@id='b_results']//h2/a")));
+
+        List<WebElement> wb = driver.findElements(By.xpath("//ol[@id='b_results']//h2/a"));
+
+        // for(WebElement link: wb){
+        //     String s = link.getAttribute("href");
+        //     String text = link.getText();
+        //     System.out.println(text + " ----> " +s);
+        // }
+        Iterator<WebElement> it = wb.iterator();
+
+        while(it.hasNext()){
+            String keyword = "Selenium";
+            // int relevantCount = 0;
+
+            WebElement link = it.next();
+            String s = link.getAttribute("href");
+            String text = link.getText();
+            // System.out.println(text + " ----> " +s);
+
+            if (text.contains(keyword) || s.contains(keyword)) {
+                // relevantCount++;
+                System.out.println("VALID: " + text );
+            } else {
+                System.out.println("NOT RELEVANT: " + text);
+            }
+            // Assert.assertTrue(relevantCount > 0,
+            //     "No relevant search results found for keyword: " + keyword);
+            }
+        }
+    
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+    }  
+}
--- practice.java
+++ practice.java
@@ -5,28 +5,32 @@ import java.util.List;
 import CodesL2.Maps.vowelfrequency;
 
 public class practice {
-    public static void callfun(String[] s){
 
-      for(int i=0; i<s.length; i++){
-        for(int j=i+1; j<s.length;j++){
-            if(s[i].length()>s[j].length()){
-                String a = s[j];
-                s[j] = s[i];
-                s[i] = a;
-            }
+    public static void callfun(int[] n){
+      int i=0;
+      int j=n.length-1;
+
+      int max =0;
+
+      while(i<j){
+        int height = Math.min(n[i], n[j]);
+        int width = j-i;
+        int area = height * width;
+        max = Math.max(max, area);
+
+        if(n[i]<n[j]){
+          i++;
+        }else{
+          j--;
         }
       }
-      List<String> ns = new ArrayList<>();
-      for(String na : s){
-        ns.add(na);
-      }
-      System.out.println(ns);    
+      System.out.println(max);
     }
 
     public static void main(String[] args) {
-        
-       String[] a = {"apple", "kiwi", "banana"};
-       callfun(a);
+        int[] n = {1,2,5,6,7,8,4,2,3,5,6,5,7,1};
+        callfun(n);
+       
     }
 
 }
